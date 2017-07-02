e77b3f473ccaf41ab0956e3a7d23a7551ced4a95
Itunuloluwa Fatoki <itunuloluwa.fatoki@andela.com>
2017-07-02 23:49:04 +0100

    feature (authentication): build authentication middleware
    Setup middleware for login, signup and protection of required routes
    
    [finishes #148201117]

---
+++ server/auth/local.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+require('dotenv').config();
+
+module.exports = {
+  encodeToken: (userId) => {
+    const payload = {
+      exp: moment().add(14, 'days').unix(),
+      iat: moment().unix(),
+      sub: userId
+    };
+    const token = jwt.sign(payload, process.env.SECRET_KEY);
+    return token;
+  },
+  decodeToken: (token, callback) => {
+    const payload = jwt.verify(token, process.env.SECRET_KEY);
+    const now = moment().unix();
+
+    // check if the token has expired
+    if (now > payload.exp) callback('token has expired.');
+    else callback(null, payload);
+  }
+};
