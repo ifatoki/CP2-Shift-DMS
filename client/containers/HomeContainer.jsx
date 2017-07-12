60da8305824622e8bd0a3c1b1d913ea8c6b6f5d8
Itunuloluwa Fatoki <itunuloluwa.fatoki@andela.com>
2017-07-12 18:53:45 +0100

    feature (login_logout): implement user signin and signout
    user is able to login with existing account details and also log out of the account successfully
    
    [finishes #148408283]

--- client/containers/HomeContainer.jsx
+++ client/containers/HomeContainer.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../actions/user';
 
 class LoginContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.logUserOut();
+  }
+
   render() {
     return (
       <div>
         <h1>
           Yah! I am logged in to the HOME PAGE
         </h1>
+        <div
+          className="ui primary submit button"
+          onClick={this.onSubmit}
+        >Log Out</div>
       </div>
     );
   }
 }
 
-export default LoginContainer;
+LoginContainer.propTypes = {
+  logUserOut: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(actionCreators, dispatch);
+export default connect(null, mapDispatchToProps)(LoginContainer);
